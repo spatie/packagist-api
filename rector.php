9d05e3b91c77e51e9dbc2b3939cc38568a7ba8e7
Michael Babker <michael@happydog.digital>
2025-03-08 15:49:30 -0500

    Add Rector config

---
+++ rector.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    ->withPreparedSets(codeQuality: true, phpunitCodeQuality: true)
+    ->withPhpSets()
+;
